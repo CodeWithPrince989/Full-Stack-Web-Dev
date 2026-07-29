e86c96e28bee8dee7f50a49bafabe8cdb41fc8cd
Prince <princekumarsingh016592@gmail.com>
2026-07-29 15:59:04 +0530

    learning js

---
+++ HTML and CSS/siteWithJs/index.js
@@ -0,0 +1,22 @@
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+}
+function reomove1Points() {
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+reomove1Points()
+reomove1Points()
+console.log(myPoints)
+
+console.log("2" + 2)
+console.log(11 + 7)
+console.log(6 + "5")
+console.log("My Points: " + 5 + 9)
+console.log(2 + 2)
+console.log("11" + "14")
